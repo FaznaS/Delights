c45fb8899ab77a9da7c8cc4179279b9064c43b62
DESKTOP-L8A9RC2\Fazna <gfsheriffdeen@gmail.com>
2025-06-19 23:55:09 +0530

    Added new page

---
+++ frontend/src/pages/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Menu = () => {
+  return (
+    <div>
+      <Header />
+      <div className='page-container'>
+        <h1>Menu</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Menu
